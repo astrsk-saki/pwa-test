7af8bc4a136d473e8e0483c45fe7f713ab8976c6
ɐmɐʞıʞɐs <sakikawa@astrsk.jp>
2018-10-23 12:06:30 +0900

    Update camera.js

--- camera.js
+++ camera.js
@@ -1,3 +1,17 @@
+var p = navigator.mediaDevices.getUserMedia({ audio: true, video: true });
+
+p.then(function(stream) {
+   var video = document.querySelector('video');
+   video.src = window.URL.createObjectURL(mediaStream);
+   video.onloadedmetadata = function(e) {
+      // Do something with the video here.
+   };
+};
+
+p.catch(function(e) { console.log(e.name); }); // always check for errors at the end.
+
+
+/*
 const medias = {audio : false, video : true},
       video  = document.getElementById("video");
 
@@ -10,3 +24,4 @@ function successCallback(stream) {
 function errorCallback(err) {
   alert(err);
 };
+*/
